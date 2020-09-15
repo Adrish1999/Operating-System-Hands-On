53da926c75657b9033abbe7ebb5a9a60c6b941df
Adrish Bose <adrishbose198@gmail.com>
2020-09-15 22:39:49 +0530

    Create swap with temp

---
+++ Shell_Assignment_01/Swap_with_temp.sh
@@ -0,0 +1,12 @@
+echo "Enter the first number: "
+read a
+echo "Enter the second number: "
+read b
+echo "Before swapping, numbers are:"
+echo "first = $a, second = $b"
+temp=$a
+a=$b
+b=$temp
+echo "After swapping, numbers are:"
+echo "first = $a, second = $b"
+
