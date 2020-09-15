6b806f52221c44726d4a39252f7502cb6877ecbf
Adrish Bose <adrishbose198@gmail.com>
2020-09-15 22:16:30 +0530

    Update Simple interest

--- Shell_Assignment_01/Simple_Interest.sh
+++ Shell_Assignment_01/Simple_Interest.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 echo -n "Enter the principal amount: "
 read p
 echo -n "Enter the rate of interest: "
 read r
 echo -n "Enter the number of years: "
 read t
-interest = $(echo "($p * $r * $t)/100"|bc)
-echo "Simple interest: " $interest
+interest=$(echo $p $r $t | awk '{ printf "%f", ($1 * $2 * $3)/100 }')
+echo "Simple interest : $interest"
