c009687791dff0bdd0a6d1e06a8245ced72e1890
Adrish <54068755+Adrish1999@users.noreply.github.com>
2020-12-14 15:30:21 +0530

    Create Orphan_01.c

---
+++ Lab_Assignment_08/Orphan_01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+        int p;
+        p = fork();
+        if(p == 0)
+        {
+                printf("\nChild process\n");
+                sleep(5);
+                printf("Orphan process\n");
+        }
+        else if(p > 0)
+        printf("\nParent proces\n");
+        return 1;
+}
