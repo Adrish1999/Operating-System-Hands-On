92525e0483fb781ab69b3317c23490fda024de92
Adrish <54068755+Adrish1999@users.noreply.github.com>
2020-12-14 15:36:08 +0530

    Create Zombie_01.c

---
+++ Lab_Assignment_08/Zombie_01.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+        int pid;
+        pid = fork();
+        if( pid == 0 )
+        {
+                printf("\nChild Process\n");
+        }
+        else if( p > 0 )
+        {
+                sleep(5);
+                printf("\nParent Process\n");
+                printf("Zombie Process\n");
+                while(1)
+                {
+                };
+                return 1;
+        }
+}
